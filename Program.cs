8e5aa89036bc4499d129157a85daff57d37b635c
muratpala007 <muratpala007@gmail.com>
2022-04-19 18:33:28 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _74DersIcIceSiniflar
+{
+    class Program
+    {
+        //Nested Classes: Ic Ice Siniflar
+        class Araba
+        {
+            public string model;
+            public Motor motor;
+            public Araba(string modeli)
+            {
+                motor = new Motor(); //Instructor metot'ta Instance olusturduk.
+            }
+            public class Motor // Ic ice sinif olusturduk. Ancak bu sinifin motorGucu field'ine ulasabilmek icin Araba sinifinde bir Instance olsuturduk.
+            {
+                public int motorGucu;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Araba a1 = new Araba("2015");
+            a1.motor.motorGucu = 1500;
+            Console.WriteLine(a1.motor.motorGucu);
+        }
+    }
+}
